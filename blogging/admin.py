0815da9b6efb0bcdb8ace7f19e899bf9659f5ef6
Dang <wei-min.dang@pse.com>
2020-11-08 22:37:41 -0800

    assignment 7 ready for review

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "posts_count")
+
+    exclude = ("posts",)
+
+    def posts_count(self, obj):
+        return str(obj.posts.count()) + " posts"
+
+class CategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "text", "author")
+    inlines = [
+        CategoryInLine,
+    ]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
